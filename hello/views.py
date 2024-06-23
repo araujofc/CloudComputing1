8d63e052a4caf91700651ed5a6ef135537547f3f
araujofc <araujofc@hotmail.com>
2024-06-23 10:11:33 -0300

    primeiro commit

--- hello/views.py
+++ hello/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello World! Django is running!")
+    return HttpResponse("Nome: Fabricio Coutinho de Araujo Disciplina:Cloud Computing & Site Reliability Engineering")
